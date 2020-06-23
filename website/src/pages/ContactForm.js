acd667348a9c87fc6b6115736df0b30de9950b9f
Marcel <marcel.riek@googlemail.com>
2020-06-23 17:42:34 +0200

    continue contact form styling, add react logic

--- website/src/pages/ContactForm.js
+++ website/src/pages/ContactForm.js
@@ -24,18 +24,35 @@ class ContactForm extends Component {
   }
 
   handleChange = event => {
-    console.log(event.target.name, event.target.value)
+    console.log(
+      "EVENT NAME:",
+      event.target.name,
+      "EVENT VALUE",
+      event.target.value
+    )
     this.setState({
       [event.target.name]: event.target.value,
     })
   }
 
-  sendMessage = event => {
+  handleCheckboxChange = event => {
+    console.log(
+      "EVENT NAME:",
+      event.target.name,
+      "EVENT VALUE",
+      event.target.value
+    )
+    this.setState({
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  handleSubmit = event => {
     event.preventDefault()
     console.log("Submit clicked")
     console.log(event)
     axios.post("https://", {
-      contactReason: this.state.contactReason,
+      contactReason: this.state.contactReason, //
       name: this.state.name, //
       firstName: this.state.firstName, //
       email: this.state.email, //
@@ -54,14 +71,22 @@ class ContactForm extends Component {
   render() {
     return (
       <MyFormContainer>
-        <form method="post" action="/contactform">
-          <label htmlFor="contact_reason">
+        <form
+          method="post"
+          action="https://formspree.io/slady@zenstudios.de "
+          onSubmit={this.handleSubmit}
+        >
+          <label htmlFor="contactReason">
             <select
-              className="input"
-              name="contact_reason"
+              className="dropdown"
+              name="contactReason"
+              value={this.state.contactReason}
+              onChange={this.handleChange}
               id="contact_reason"
-              value=""
             >
+              <option value="" selected>
+                Kontaktgrund
+              </option>
               <option value="music_lesson">Musikunterricht</option>
               <option value="rehearsal_room">Proberaum</option>
               <option value="recording">Aufnahmen</option>
@@ -74,6 +99,8 @@ class ContactForm extends Component {
               type="text"
               name="name"
               id="name"
+              value={this.state.name}
+              onChange={this.handleChange}
               placeholder="Name*"
               required
             />
@@ -82,8 +109,10 @@ class ContactForm extends Component {
             <input
               className="input"
               type="text"
-              name="first_name"
+              name="firstName"
               id="first_name"
+              value={this.state.firstName}
+              onChange={this.handleChange}
               placeholder="Vorname*"
               required
             />
@@ -92,8 +121,10 @@ class ContactForm extends Component {
             <input
               className="input"
               type="email"
-              name="email"
+              name="_replyto"
               id="email"
+              value={this.state.email}
+              onChange={this.handleChange}
               placeholder="E-Mail*"
               required
             />
@@ -104,6 +135,8 @@ class ContactForm extends Component {
               type="tel"
               name="phone"
               id="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
               placeholder="Telefon (optional)"
             />
           </label>
@@ -112,6 +145,8 @@ class ContactForm extends Component {
               name="message"
               id="message"
               rows="5"
+              value={this.state.message}
+              onChange={this.handleChange}
               placeholder="Nachricht*"
               required
             />
@@ -122,11 +157,19 @@ class ContactForm extends Component {
               type="text"
               name="instrument"
               id="instrument"
+              value={this.state.instrument}
+              onChange={this.handleChange}
               placeholder="Instrument"
             />
           </label>
           <label htmlFor="band_members">
-            <select className="dropdown" name="band_members" id="band_members">
+            <select
+              className="dropdown"
+              name="nrOfBandMembers"
+              id="band_members"
+              value={this.state.nrOfBandMembers}
+              onChange={this.handleChange}
+            >
               <option value="" selected>
                 Anzahl Bandmitglieder
               </option>
@@ -138,8 +181,10 @@ class ContactForm extends Component {
           <label htmlFor="practice_days">
             <select
               className="dropdown"
-              name="practice_days"
+              name="nrOfPracticeDaysPerWeek"
               id="practice_days"
+              value={this.state.nrOfPracticeDaysPerWeek}
+              onChange={this.handleChange}
             >
               <option value="" selected>
                 Probentage/Woche
@@ -147,15 +192,20 @@ class ContactForm extends Component {
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
-              <option value="3+">3+</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
             </select>
           </label>
           <label htmlFor="musical_style">
             <input
               className="input"
               type="text"
-              name="musical_style"
+              name="musicalStyle"
               id="musical_style"
+              value={this.state.musicalStyle}
+              onChange={this.handleChange}
               placeholder="Musikstil"
             />
           </label>
@@ -165,13 +215,20 @@ class ContactForm extends Component {
                 type="checkbox"
                 id="recording"
                 name="recording"
-                value="recording"
+                checked={this.state.recording}
+                onChange={this.handleCheckboxChange}
               />
               <label htmlFor="recording">Aufnahme</label>
             </p>
 
             <p>
-              <input type="checkbox" id="mixing" name="mixing" value="mixing" />
+              <input
+                type="checkbox"
+                id="mixing"
+                name="mixing"
+                checked={this.state.mixing}
+                onChange={this.handleCheckboxChange}
+              />
               <label htmlFor="mixing">Mixing</label>
             </p>
 
@@ -179,13 +236,14 @@ class ContactForm extends Component {
               <input
                 type="checkbox"
                 id="videoshoot"
-                name="videoshoot"
-                value="videoshoot"
+                name="videoShoot"
+                checked={this.state.videoShoot}
+                onChange={this.handleCheckboxChange}
               />
               <label htmlFor="videoshoot">Videodreh</label>
             </p>
 
-            <span>* Pflichtfeld</span>
+            <span className="mandatory-info">* Pflichtfeld</span>
             <button className="btn btn-primary" type="submit">
               Senden
             </button>
@@ -197,11 +255,18 @@ class ContactForm extends Component {
 }
 
 const MyFormContainer = styled(Container)`
+  /* form container */
   background-color: #eeeeee;
   height: 100%;
   width: 350px;
+  padding-top: 30px;
+  margin: 50px auto;
+  border: 1px solid #888888;
+  border-radius: 3px;
   display: flex;
   justify-content: center;
+
+  /* form */
   form {
     width: 100%;
     margin-top: 30px;
@@ -211,41 +276,133 @@ const MyFormContainer = styled(Container)`
     flex-direction: column;
   }
 
+  /* message field */
   form textarea {
     width: 100%;
-    height: 150px;
+    height: 200px;
     padding-left: 10px;
+    border: 1px solid #888888;
+    border-radius: 3px;
+    box-sizing: border-box;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+    display: flex;
+    align-items: center;
+    color: #888888;
+    background: #ffffff;
   }
 
+  /* input fields */
   .input {
     width: 100%;
     height: 40px;
     padding-left: 10px;
+    border: 1px solid #888888;
+    border-radius: 3px;
+    box-sizing: border-box;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+    display: flex;
+    align-items: center;
+    color: #888888;
+    background: #ffffff;
   }
 
+  ${"" /* input::placeholder {
+    color: red;
+  } */}
+
+  /* dropdown fields */
   .dropdown {
     width: 100%;
     height: 40px;
+    padding-left: 10px;
+    ${"" /* -webkit-appearance: none; */}
+    border: 1px solid #888888;
+    box-sizing: border-box;
+    border-radius: 3px;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+    display: flex;
+    align-items: center;
+    color: #888888;
+    background: #ffffff;
   }
 
+  ${"" /* .dropdown select option {
+    color: red;
+  }
+  select:not(:checked) {
+    color: gray;
+  } */}
+
+  /* checkbox container */
   .checkboxes {
     width: 100%;
+    margin-top: 20px;
     display: flex;
     justify-content: center;
     align-items: flex-start;
     flex-direction: column;
   }
 
-  .checkboxes label {
-    margin-left: 10px;
+  /* checkbox icon styling */
+  input[type="checkbox"] {
+    display: none;
   }
 
-  .checkboxes p,
-  span {
+  input[type="checkbox"] + label::before {
+    width: 15px;
+    height: 15px;
+    margin-top: 5px;
+    margin-left: 5px;
+    border-radius: 2px;
+    border: 2px solid #cf6e14;
+    background-color: #fff;
+    display: block;
+    content: "";
+    float: left;
+    margin-right: 5px;
+  }
+  input[type="checkbox"]:checked + label::before {
+    box-shadow: inset 0px 0px 0px 1px #fff;
+    background-color: #e98120;
+  }
+
+  /* checkboxes including text */
+  .checkboxes p {
     width: 100%;
     height: 40px;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+    color: #141414;
   }
 
+  /* mandatory info line */
+  .mandatory-info {
+    width: 100%;
+    height: 40px;
+    font-family: Source Sans Pro;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 23px;
+    color: #888888;
+  }
+
+  /* form submit button */
   .checkboxes button {
     font-family: Source Sans Pro;
     font-style: normal;
@@ -262,7 +419,6 @@ const MyFormContainer = styled(Container)`
     display: flex;
     justify-content: center;
     align-items: center;
-    color: #ffffff;
     background: linear-gradient(
         180deg,
         rgba(0, 0, 0, 0) 0%,
@@ -275,15 +431,27 @@ const MyFormContainer = styled(Container)`
     border-radius: 3px;
     &:hover {
       background-color: ${props => props.theme.blockCTAHover};
-      border: 2px solid #d6300a;
+      border: 3px solid #d6300a;
     }
+  }
+`
+
+const HeadlineWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 120px;
+  h2 {
+    color: #fff;
   }
 `
 
 const contactFormPage = props => {
   console.log("test")
   return (
-    <Layout>
+    <Layout showHeader showLogo>
+      <HeadlineWrapper>
+        <h2>Schreibe uns eine Nachricht!</h2>
+      </HeadlineWrapper>
       <ContactForm></ContactForm>
     </Layout>
   )
