4b93538e410790a7ce6da0ff259a2f91f06772e5
Jonathan Saudhof <Jonathan@jonathansaudhof.de>
2020-06-29 11:29:35 +0200

    added preview plugin for sanity

--- sanity/resolveProductionUrl.js
+++ sanity/resolveProductionUrl.js
@@ -1,7 +1,14 @@
+const isDraft = (id) => id.includes("drafts");
+
 export default function resolveProductionUrl(document) {
   console.log(document);
-  if (document._type === "page") {
+  if (
+    document._type === "page" ||
+    document._type === "post" ||
+    document._type === "landingPageSection"
+  ) {
     // Then we get its ID
+    console.log(document._type);
     let id = document._id;
     // if it's a draft, we split its _id with the "drafts." substring, which will return an array,
     // and get the second item in it, which will be the isolated _id without "drafts."
@@ -11,7 +18,7 @@ export default function resolveProductionUrl(document) {
     // And return a template string reflecting the URL structure we want. In this case, we're doing a
     // simple conditional to return '&isDraft=true' as a param for drafts as we'll query them
     // differently in the front-end
-    return `https://jonathansaudhof.de/preview?pageId=${id}${
+    return `http://localhost:8000/preview?type=${document._type}&pageId=${id}${
       isDraft(document._id) ? "&isDraft=true" : ""
     }`;
   }
